30f3843751f2fe43369e5bdf5ad24f72562df330
Francisco José García García <frangarcj@gmail.com>
2024-10-22 19:15:31 +0200

    fix: cambio de autor

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $portada = [
 "titulo" => "Portada",
-"autor" => "ggf906",
+"autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
 ?>
